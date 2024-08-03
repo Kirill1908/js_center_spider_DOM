52f2040719aad3d4008f576fe68d52ace260fdd5
Kirill Gasan <dp160190gkj@gmail.com>
2024-08-03 16:41:39 +0300

    make build

---
+++ index.bab30fa9.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".wall"),t=e.clientHeight/2,c=e.clientWidth/2,n=document.querySelector(".spider"),i=n.getBoundingClientRect().height/2,l=n.getBoundingClientRect().width/2;n.style.top="".concat(t-i,"px"),n.style.left="".concat(c-l,"px");
+//# sourceMappingURL=index.bab30fa9.js.map
